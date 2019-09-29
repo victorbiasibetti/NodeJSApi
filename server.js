86749a8def790e9deb0bc48ba87c7cb32580e23e
Victor Biasibetti <victorbiasibetti7@gmail.com>
2019-09-29 17:29:51 -0300

    add console log

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 //const mongoose  = require('mongoose');
-const requireDir = require('require-dir')
-const cors = require('cors')
+//const requireDir = require('require-dir')
+//const cors = require('cors')
+
+const port = process.env.PORT || 3000
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+//app.use(cors());
 /*
 mongoose.connect('mongodb://localhost:27017/nodeapi', 
 { 
@@ -23,4 +25,10 @@ app.use('/', (req, res) => {
     return res.send("Hello World");
 })
 
-app.listen(3000);
+app.listen(port, err => {
+    if (!err) {
+      console.log('server listening on port', port)
+    } else {
+      console.log(err)
+    }
+  });
