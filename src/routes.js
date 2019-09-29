89c989b31d3a84cc26fa84ea359f2a8e191c5898
Victor Biasibetti <victorbiasibetti7@gmail.com>
2019-09-29 16:29:39 -0300

    add value to default route

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,10 @@ const routes = express.Router();
 
 const ProductController = require('./controller/ProductController')
 
+routes.get('/', (req,res)=>{
+    return res.json({"HW" : "Hello World"});
+})
+
 routes.get("/products", ProductController.index)
 routes.post("/products", ProductController.store)
 routes.get("/products/:id", ProductController.show)
