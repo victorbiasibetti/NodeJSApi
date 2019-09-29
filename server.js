b2708060b51f4963c28b5b84dcc1260df9144113
Victor Biasibetti <victorbiasibetti7@gmail.com>
2019-09-29 16:22:11 -0300

    first commit
    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose  = require('mongoose');
+const requireDir = require('require-dir')
+const cors = require('cors')
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect('mongodb://localhost:27017/nodeapi', 
+{ 
+    useUnifiedTopology: true, 
+    useNewUrlParser: true 
+})
+
+requireDir('./src/models')
+
+const Product = mongoose.model('Product')
+
+app.use('/api', require('./src/routes'))
+
+app.listen(3000);
