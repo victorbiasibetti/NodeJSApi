5214b913dd0e0901a01ac0089aa258fd0e247601
Victor Biasibetti <victorbiasibetti7@gmail.com>
2019-09-29 16:41:08 -0300

    teste no heroku

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 const express = require('express');
-const mongoose  = require('mongoose');
+//const mongoose  = require('mongoose');
 const requireDir = require('require-dir')
 const cors = require('cors')
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-
+/*
 mongoose.connect('mongodb://localhost:27017/nodeapi', 
 { 
     useUnifiedTopology: true, 
     useNewUrlParser: true 
+})*/
+
+//requireDir('./src/models')
+
+//const Product = mongoose.model('Product')
+
+//app.use('/api', require('./src/routes'))
+
+app.use('/', (req, res) => {
+    return res.json({"HW" : "Hello World"});
 })
-
-requireDir('./src/models')
-
-const Product = mongoose.model('Product')
-
-app.use('/api', require('./src/routes'))
 
 app.listen(3000);
